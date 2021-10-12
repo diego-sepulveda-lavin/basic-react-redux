9a03ba1dc630e16b557f67ba133b4a55d136fd23
Diego Sepulveda <diegosl@hotmail.cl>
2021-10-12 19:16:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch()
+    const counter = useSelector(state => state.counter)
+
+    const handleDecrease = () => {
+        dispatch({type:"decrease"})
+    }
+
+    const handleIncrease = () => {
+        dispatch({type:"increase"})
+    }
+
+    return (
+        <div>
+            <h1>My counter</h1>
+            <p>{counter}</p>
+            <button onClick={handleDecrease}>Decrease</button>
+            <button onClick={handleIncrease}>Increase</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import {createStore} from "redux"
+
+const counterInitialValue = {counter: 0}
+
+const counterReducer = (state = counterInitialValue, action) => {
+    if (action.type === "increase") {
+        return {
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === "decrease") {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state
+}
+
+const store = createStore(counterReducer)
+
+export default store
+
+// const counterSubscriber = () => {
+//     const latestState = store.getState()
+//     console.log(latestState)
+// }
+//
+// store.subscribe(counterSubscriber)
+
